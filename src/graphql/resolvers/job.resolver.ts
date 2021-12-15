373df1fb28b270b369677be33ed8961a08783981
Assaf Chamoy <assafchamoy97@gmail.com>
2021-12-15 09:50:25 +0200

    feat: updated job model and mock data (#38)
    * feat: updated job model and mock data
    
    * feat: jobs mock data changes

--- src/graphql/resolvers/job.resolver.ts
+++ src/graphql/resolvers/job.resolver.ts
@@ -7,109 +7,8 @@ import { transform, mapKeys, mapValues, get } from 'lodash';
 import { Services } from '../../common/constants';
 import { requestHandler } from '../../utils';
 import { JobsSearchParams, JobUpdateData } from '../inputTypes';
-import { Job, Status } from '../job';
-
-const MOCK_JOBS_DATA = [
-  {
-    id: '108f23b2-a0af-433e-be30-981d89e10656',
-    resourceId: '1_discete_ext',
-    version: 'string',
-    type: 'Discrete-Tiling',
-    description: 'string',
-    parameters: {},
-    status: Status.Pending,
-    percentage: 0,
-    reason: 'string',
-    isCleaned: false,
-    priority: 0,
-    tasks: [
-      {
-        id: 'cbe00422-acf4-4b81-9a7c-90a9a51bdf2b',
-        jobId: '108f23b2-a0af-433e-be30-981d89e10656',
-        type: 'string',
-        description: 'string',
-        parameters: {},
-        status: Status.Pending,
-        percentage: 0,
-        reason: 'string',
-        attempts: 1,
-        created: new Date('2021-05-24T05:33:22.199Z'),
-        updated: new Date('2021-05-24T08:48:16.411Z'),
-      },
-      {
-        id: 'cbe00422-acf4-4b81-9a7c-90a9a51bdf2c',
-        jobId: '108f23b2-a0af-433e-be30-981d89e10656',
-        type: 'string',
-        description: 'string',
-        parameters: {},
-        status: Status.Completed,
-        percentage: 0,
-        reason: 'string',
-        attempts: 1,
-        created: new Date('2021-05-24T05:33:22.199Z'),
-        updated: new Date('2021-05-24T08:48:16.411Z'),
-      },
-      {
-        id: 'cbe00422-acf4-4b81-9a7c-90a9a51bdf2d',
-        jobId: '108f23b2-a0af-433e-be30-981d89e10656',
-        type: 'string',
-        description: 'string',
-        parameters: {},
-        status: Status.Failed,
-        percentage: 0,
-        reason: 'string',
-        attempts: 1,
-        created: new Date('2021-05-24T05:33:22.199Z'),
-        updated: new Date('2021-05-24T08:48:16.411Z'),
-      },
-    ],
-    created: new Date('2021-05-24T05:33:22.199Z'),
-    updated: new Date('2021-05-24T05:33:22.199Z'),
-  },
-  {
-    id: '5907d4de-3afc-41f2-bc32-a904e34adbc0',
-    resourceId: '2_discete_ext',
-    version: 'string',
-    type: '3D',
-    description: 'string',
-    parameters: {},
-    status: Status.Pending,
-    percentage: 0,
-    reason: 'string',
-    isCleaned: false,
-    priority: 0,
-    tasks: [
-      {
-        id: 'df9c0c87-44b8-4a6c-8585-d47ef02ccd77',
-        jobId: '5907d4de-3afc-41f2-bc32-a904e34adbc0',
-        type: 'string',
-        description: 'string',
-        parameters: {},
-        status: Status.Pending,
-        percentage: 0,
-        reason: 'string',
-        attempts: 0,
-        created: new Date('2021-05-24T05:33:51.990Z'),
-        updated: new Date('2021-05-24T05:33:51.990Z'),
-      },
-      {
-        id: 'df9c0c87-44b8-4a6c-8585-d47ef02ccd78',
-        jobId: '5907d4de-3afc-41f2-bc32-a904e34adbc0',
-        type: 'string',
-        description: 'string',
-        parameters: {},
-        status: Status.InProgress,
-        percentage: 0,
-        reason: 'string',
-        attempts: 0,
-        created: new Date('2021-05-24T05:33:51.990Z'),
-        updated: new Date('2021-05-24T05:33:51.990Z'),
-      },
-    ],
-    created: new Date('2021-05-24T05:33:51.990Z'),
-    updated: new Date('2021-05-24T05:33:51.990Z'),
-  },
-];
+import { Job } from '../job';
+import { MOCK_JOBS_DATA } from './MOCK_JOBS_DATA';
 
 @Resolver()
 export class JobResolver {
@@ -132,8 +31,8 @@ export class JobResolver {
     try {
       // TODO: use a real call
       const data = await Promise.resolve(this.getJobs(params));
-      return this.transformRecordsToEntity(data);
       // const data = await Promise.resolve(MOCK_JOBS_DATA);
+      return this.transformRecordsToEntity(data);
       // return data;
     } catch (err) {
       this.logger.error(err);
