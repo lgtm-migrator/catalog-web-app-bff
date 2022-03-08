0005f4a312604f8150d06cffc3cd9ace7549890c
Assaf Chamoy <assafchamoy97@gmail.com>
2022-03-08 12:20:40 +0200

    feat: csw access token (#67)

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosRequestConfig, AxiosResponse, Method } from 'axios';
+import config from 'config';
 
 export enum CatalogRecordItems {
   RASTER = 'RASTER',
@@ -14,4 +15,25 @@ export const requestHandler = async (url: string, method: string, params: AxiosR
     .catch((error) => {
       throw error;
     });
+};
+
+export const requestHandlerWithToken = async (url: string, method: string, params: AxiosRequestConfig): Promise<AxiosResponse> => {
+  const injectionType = config.get<string>('accessToken.injectionType');
+  const attributeName = config.get<string>('accessToken.attributeName');
+  const tokenValue = config.get<string>('accessToken.tokenValue');
+  const reqConfig = { ...params };
+
+  if (injectionType.toLowerCase() === 'header') {
+    reqConfig.headers = {
+      ...reqConfig.headers,
+      [attributeName]: tokenValue,
+    } as Record<string, unknown>;
+  } else if (injectionType.toLowerCase() === 'queryparam') {
+    reqConfig.params = {
+      ...reqConfig.params,
+      [attributeName]: tokenValue,
+    } as Record<string, unknown>;
+  }
+
+  return requestHandler(url, method, reqConfig);
 };
