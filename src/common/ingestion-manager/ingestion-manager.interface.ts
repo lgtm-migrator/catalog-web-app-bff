1114e27d835e70323e47cd5990a8101baf94fe8d
assafchamoy <assafchamoy97@gmail.com>
2022-10-26 19:18:35 +0300

    feat: added productOpacity field to IngestionRasterData input type

--- src/common/ingestion-manager/ingestion-manager.interface.ts
+++ src/common/ingestion-manager/ingestion-manager.interface.ts
@@ -1,3 +1,4 @@
+import { registerEnumType } from 'type-graphql';
 import { IngestionData } from '../../graphql/inputTypes';
 
 // eslint-disable-next-line import/exports-last
@@ -5,3 +6,13 @@ export interface IIngestionManagerService {
   ingest: (data: IngestionData) => Promise<IngestionData>;
   updateGeopkg?: (data: IngestionData) => Promise<IngestionData | null>;
 }
+
+// IngestionData related types
+
+// eslint-disable-next-line import/exports-last
+export enum ProductOpacity {
+  TRANSPARENT = 'transparent',
+  OPAQUE = 'opaque',
+}
+
+registerEnumType(ProductOpacity, { name: 'ProductOpacity' });
